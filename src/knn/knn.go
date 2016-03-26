cdaadc25a4d1eb19a26e253cf170cf2f8e616003
lcbluestorm <lcbluestorm@gmail.com>
2016-03-26 23:38:20 +0800

    Heap surpport Item{value,label}

--- src/knn/knn.go
+++ src/knn/knn.go
@@ -1,24 +1,38 @@
 package knn
 
 import (
+	"../datastructure"
+
 	"fmt"
 )
 
 type KnnClassifier struct {
-	data []*Model
-	k    int
+	dataSet *[]*Model
+	k       int
 }
 
-func NewKnnClassifier(data []*Model, k int) *KnnClassifier {
-	return &KnnClassifier{data: data, k: k}
+func NewKnnClassifier(dataSet *[]*Model, k int) KnnClassifier {
+	return KnnClassifier{dataSet: data, k: k}
 }
 
-func (k KnnClassifier) Classify(m Model) string {
-	topK := make([]Model, k.k)
+func (knn KnnClassifier) Classify(m *Model) string {
+
+	maxHeap := datastructure.NewMaxHeap(interface{})
 	var label string
-	for i, model := range k.data {
-		fmt.Println("i=%s,model=%s", i, model)
-
+	for i, model := range knn.dataSet {
+		distance := m.GetDistance(*model)
+		if maxHeap.GetSize() < knn.k {
+			item := datastructure.NewItem(distance, model)
+			maxHeap.Add(item)
+		} else {
+			maxHeap.InsertValue(distance, model)
+		}
 	}
+	values := maxHeap.GetValues()
+	counts := make(map[string]int, knn.k)
+	for _, value := range values {
+		v := value.(Item)
+	}
+
 	return label
 }
